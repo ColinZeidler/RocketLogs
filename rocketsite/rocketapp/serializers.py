efdaacdff7831e9a5f7222d4362d00776ac2926e
Colin Zeidler <c.zeidler.wc@gmail.com>
2017-04-23 23:20:48 -0400

    Creating new users availble by api at /users

--- rocketsite/rocketapp/serializers.py
+++ rocketsite/rocketapp/serializers.py
@@ -10,6 +10,16 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
         fields = ('url', 'username', 'password', 'email', 'groups', 'rockets', 'flights')
